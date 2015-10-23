dda3fa7c2ab937b553680116f2da0e3ffbe6cf1c
Avi Cavale <avi@shippable.com>
2015-10-23 11:12:14 -0700

    removing global node install

--- install.sh
+++ install.sh
@@ -1,25 +1,8 @@
 #!/bin/bash -e
-# install nvm:
-curl https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 
-# Set NVM_DIR so the installations go to the right place
-export NVM_DIR="/root/.nvm"
+#base image already has node and nvm installed globally...
 
-# add source of nvm to .bashrc - allows user to use nvm as a command
-echo "source ~/.nvm/nvm.sh" >> .bashrc
-
-for file in /tmp/version/*;
-do
-  $file
-done
-
-# tell nvm to use this version as default
-. /root/.nvm/nvm.sh && nvm alias default 0.12
-
-echo "================= Updating Ubuntu nodejs ==================="
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
+echo "================= Updating global nodejs packages ==================="
 sudo npm install -g grunt-cli mocha vows phantomjs casperjs;
 
 echo "================ Installing selenium =================="
@@ -28,3 +11,8 @@ cd /srv && sudo wget http://selenium-release.storage.googleapis.com/2.48/seleniu
 
 echo "=============== Installing bower globally ============="
 sudo npm install -g bower
+
+for file in /tmp/version/*;
+do
+  $file
+done
